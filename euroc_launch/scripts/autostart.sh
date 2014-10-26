e3fac8c786148d75f1f5c0b360ef7a6b653ea8e6
Moritz Horstmann <mhorst@tzi.de>
2014-10-26 19:11:36 +0100

    Upload logs, shutdown after finish.

--- euroc_launch/scripts/autostart.sh
+++ euroc_launch/scripts/autostart.sh
@@ -9,6 +9,9 @@ source $ROOT/euroc_ws/devel/setup.bash
 cd $ROOT/euroc_ws
 wstool update
 catkin_make
-if [ "$1" == "--with-server" ] then
-	rosrun euroc_launch install_integrationtest
-fi
+rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1
+LOGNAME=logs`date +%Y-%m-%d-%H-%M-%S`
+tar cvfz $LOGNAME /tmp/euroc*
+curl -i -F data=@$LOGNAME https://www.mho-service.net/uni/euroc/imp.php?secret=79f9fc3df583a0ae06def20b000e8130
+rm $LOGNAME
+dbus-send --system --print-reply --dest=org.freedesktop.ConsoleKit /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop
