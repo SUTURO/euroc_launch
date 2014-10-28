b598ae5ec008c0a691e36c20043097142ffd2ba2
Moritz Horstmann <mhorst@tzi.de>
2014-10-28 20:48:37 +0100

    Log git state.

--- euroc_launch/scripts/autostart.sh
+++ euroc_launch/scripts/autostart.sh
@@ -12,6 +12,22 @@ catkin_make
 #rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1,task1_v2,task1_v3,task2_v1_1,task2_v1_2,task2_v1_3,task2_v2_1,task2_v2_2,task2_v2_3,task2_v3_1,task2_v3_2,task2_v3_3,task3_v1,task3_v2,task3_v3,task4_v1_1,task4_v1_2,task4_v1_3,task4_v2_1,task4_v2_2,task4_v2_3,task4_v3_1,task4_v3_2,task4_v3_3
 rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1,task2_v1_1,task3_v1,task4_v1_1
 ps -AfH >/tmp/euroc_c2/processlist.log
+echo "###Perception GIT" >>/tmp/euroc_c2/git.log
+cd $ROOT/euroc_ws/src/euroc_perception
+git log -n1 >>/tmp/euroc_c2/git.log
+echo "###Manipulation GIT" >>/tmp/euroc_c2/git.log
+cd $ROOT/euroc_ws/src/euroc_manipulation
+git log -n1 >>/tmp/euroc_c2/git.log
+echo "###Planning GIT" >>/tmp/euroc_c2/git.log
+cd $ROOT/euroc_ws/src/euroc_planning
+git log -n1 >>/tmp/euroc_c2/git.log
+echo "###Msgs GIT" >>/tmp/euroc_c2/git.log
+cd $ROOT/euroc_ws/src/euroc_msgs
+git log -n1 >>/tmp/euroc_c2/git.log
+echo "###Launch GIT" >>/tmp/euroc_c2/git.log
+cd $ROOT/euroc_ws/src/euroc_launch
+git log -n1 >>/tmp/euroc_c2/git.log
+cd $ROOT
 LOGNAME=logs`date +%Y-%m-%d-%H-%M-%S`
 tar cvfz $LOGNAME /tmp/euroc*
 curl -i -F data=@$LOGNAME https://www.mho-service.net/uni/euroc/imp.php?secret=79f9fc3df583a0ae06def20b000e8130
