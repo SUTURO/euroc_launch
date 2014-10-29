9e3398b5ff3a7d50be61dcc18619bbf8d1dca7f3
Moritz Horstmann <mhorst@tzi.de>
2014-10-29 18:56:43 +0100

    Update tasks in separate file, so updates are applied immediately.

--- euroc_launch/scripts/autostart.sh
+++ euroc_launch/scripts/autostart.sh
@@ -9,10 +9,7 @@ source $ROOT/euroc_ws/devel/setup.bash
 cd $ROOT/euroc_ws
 wstool update
 catkin_make
-#rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1,task1_v2,task1_v3,task2_v1_1,task2_v1_2,task2_v1_3,task2_v2_1,task2_v2_2,task2_v2_3,task2_v3_1,task2_v3_2,task2_v3_3,task3_v1,task3_v2,task3_v3,task4_v1_1,task4_v1_2,task4_v1_3,task4_v2_1,task4_v2_2,task4_v2_3,task4_v3_1,task4_v3_2,task4_v3_3
-#rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1,task2_v1_1,task3_v1,task4_v1_1
-rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1,task1_v2,task1_v3,task2_v1_1,task2_v1_2,task2_v1_3,task2_v2_1,task2_v2_2,task2_v2_3,task2_v3_1,task2_v3_2,task2_v3_3,task3_v1,task3_v2,task3_v3
-ps -AfH >/tmp/euroc_c2/processlist.log
+rosrun euroc_launch starttasks.sh
 echo "###Perception GIT" >>/tmp/euroc_c2/git.log
 cd $ROOT/euroc_ws/src/euroc_perception
 git log -n1 >>/tmp/euroc_c2/git.log
