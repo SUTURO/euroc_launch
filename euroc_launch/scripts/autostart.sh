f306b79a049cb030d2e1633e69f1a400fc9f2c56
Moritz Horstmann <mhorst@tzi.de>
2014-10-31 15:27:53 +0100

    More logs.

--- euroc_launch/scripts/autostart.sh
+++ euroc_launch/scripts/autostart.sh
@@ -8,9 +8,16 @@ source /opt/euroc_c2s1/ros/install/setup.bash
 source $ROOT/euroc_ws/devel/setup.bash
 mkdir -p /tmp/euroc_c2
 cd $ROOT/euroc_ws
+echo -n Updating...
 wstool update 2>1 >/tmp/euroc_c2/update.log
+echo " done"
+echo -n Building...
 catkin_make 2>1 >/tmp/euroc_c2/build.log
+echo " done"
+echo -n Executing tasks...
 rosrun euroc_launch starttasks.sh
+echo " done"
+echo -n Gathering and uploading logs...
 echo "###Perception GIT" >>/tmp/euroc_c2/git.log
 cd $ROOT/euroc_ws/src/euroc_perception
 git log -n1 >>/tmp/euroc_c2/git.log
@@ -31,4 +38,7 @@ LOGNAME=logs`date +%Y-%m-%d-%H-%M-%S`
 tar cvfz $LOGNAME /tmp/euroc*
 curl -i -F data=@$LOGNAME https://www.mho-service.net/uni/euroc/imp.php?secret=79f9fc3df583a0ae06def20b000e8130
 rm $LOGNAME
+echo " done"
+echo Terminating in 10 seconds!
+sleep 10
 dbus-send --system --print-reply --dest=org.freedesktop.ConsoleKit /org/freedesktop/ConsoleKit/Manager org.freedesktop.ConsoleKit.Manager.Stop
