98983f912e76a8f637d5272c09b2006e1eea8213
Moritz Horstmann <mhorst@tzi.de>
2014-11-02 15:19:58 +0100

    Use all tasks from test evaluation again

--- euroc_launch/scripts/starttasks.sh
+++ euroc_launch/scripts/starttasks.sh
@@ -3,9 +3,7 @@
 # Dont use v3 tasks, these are just slow versions of v2 tasks
 
 # Use the following for test evaluation:
-# rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1,task2_v1_1,task2_v1_2,task2_v1_3,task3_v1,task4_v1_1,task4_v1_2,task4_v1_3,task6_v1
-
-rosrun suturo_planning_startup start_complete_demo.py --tasks=task4_v1_3
+rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1,task2_v1_1,task2_v1_2,task2_v1_3,task3_v1,task4_v1_1,task4_v1_2,task4_v1_3,task6_v1
 
 # Use the following for final evaluation
 # rosrun suturo_planning_startup start_complete_demo.py
