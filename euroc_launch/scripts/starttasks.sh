8c1edc5cbdc4e0c446c68d336a5beeeca45b3b87
Moritz Horstmann <mhorst@tzi.de>
2014-11-04 15:44:52 +0100

    Add v2 tasks.

--- euroc_launch/scripts/starttasks.sh
+++ euroc_launch/scripts/starttasks.sh
@@ -3,7 +3,7 @@
 # Dont use v3 tasks, these are just slow versions of v2 tasks
 
 # Use the following for test evaluation:
-timeout --foreground -s SIGINT 3h rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1,task2_v1_1,task2_v1_2,task2_v1_3,task3_v1,task4_v1_1,task4_v1_2,task4_v1_3,task6_v1
+timeout --foreground -s SIGINT 6h rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1,task2_v1_1,task2_v1_2,task2_v1_3,task3_v1,task4_v1_1,task4_v1_2,task4_v1_3,task6_v1,task1_v2,task2_v2_1,task2_v2_2,task2_v2_3,task3_v2,task4_v2_1,task4_v2_2,task4_v2_3,task6_v2
 if [ "$?" == "124" ]
 	then echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
 	echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
