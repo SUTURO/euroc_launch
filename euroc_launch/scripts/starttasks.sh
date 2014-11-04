af4459b97e21e983404d713069eadcc51413b6cc
Moritz Horstmann <mhorst@tzi.de>
2014-11-04 13:30:57 +0100

    Terminate test after 3 hours.

--- euroc_launch/scripts/starttasks.sh
+++ euroc_launch/scripts/starttasks.sh
@@ -3,7 +3,13 @@
 # Dont use v3 tasks, these are just slow versions of v2 tasks
 
 # Use the following for test evaluation:
-rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1,task2_v1_1,task2_v1_2,task2_v1_3,task3_v1,task4_v1_1,task4_v1_2,task4_v1_3,task6_v1
-
+timeout --foreground -s SIGINT 3h rosrun suturo_planning_startup start_complete_demo.py --tasks=task1_v1,task2_v1_1,task2_v1_2,task2_v1_3,task3_v1,task4_v1_1,task4_v1_2,task4_v1_3,task6_v1
+if [ "$?" == "124" ]
+	then echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+	echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+	echo \#\#\#\# Execution of start_complete_demo.py aborted after three hours. \#\#\#\#
+	echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+	echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+fi
 # Use the following for final evaluation
 # rosrun suturo_planning_startup start_complete_demo.py
