49487725b7dc7f2b2cbef73db7853a629b52e19e
Moritz Horstmann <mhorst@tzi.de>
2014-11-12 01:47:26 +0100

    Revert "Prepare for delivery."
    This reverts commit 268e48d2b241245ddbbc4b7be48e9aea4f5facbe.

--- euroc_launch/scripts/starttasks.sh
+++ euroc_launch/scripts/starttasks.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+if [ "$TASKS" == "" ]
+	then TASKS=task1_v1,task2_v1_1,task2_v1_2,task2_v1_3,task3_v1,task4_v1_1,task4_v1_2,task4_v1_3,task6_v1,task1_v2,task2_v2_1,task2_v2_2,task2_v2_3,task3_v2,task4_v2_1,task4_v2_2,task4_v2_3,task6_v2
+fi
+if [ "$TIMEOUT" == "" ]
+	then TIMEOUT=6h
+fi
+
+timeout --foreground -s SIGUSR2 -k 100s $TIMEOUT rosrun suturo_planning_startup start_complete_demo.py --tasks=$TASKS
+if [ "$?" == "124" ] || [ "$?" == "137" ]
+	then echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+	echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+	echo \#\#\#\# Execution of start_complete_demo.py aborted by timeout. \#\#\#\#
+	echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+	echo \#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#
+fi
+
 # Use the following for final evaluation
-rosrun suturo_planning_startup start_complete_demo.py
+# rosrun suturo_planning_startup start_complete_demo.py
